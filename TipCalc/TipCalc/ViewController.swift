75f570afa8317a43492aeae1d1fde3f754b7aaf6
LuisV <lvalencia@csumb.edu>
2019-01-23 15:40:53 -0800

    Add files via upload

---
+++ TipCalc/TipCalc/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TipCalc
+//
+//  Created by Luis Valencia on 1/23/19.
+//  Copyright © 2019 Luis Valencia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func OnTap(_ sender: Any) {
+    }
+    
+}
+
